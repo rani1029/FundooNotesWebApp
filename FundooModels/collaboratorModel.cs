ab157555e07025c2e43c8a9869d13db80b51fbeb
rani1029 <ranig1029@gmail.com>
2021-12-08 07:44:59 +0530

    [Add]:collaborator model class added

---
+++ FundooModels/collaboratorModel.cs
@@ -0,0 +1,22 @@
+﻿using FundooModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FundooModels
+{
+    public class collaboratorModel
+    {
+        [Key]
+        public int CollaboratorID { get; set; }
+
+        [ForeignKey("NotesModel")]
+        public int NoteId { get; set; }
+
+        public NoteModel noteModel { get; set; }
+
+        public string CollaboratorEmail { get; set; }
+    }
+}
